c4cd8aff4d6187a55923c770bc6c0ef4df6d32d8
margot.rasamy <rasamymargot@gmail.com>
2021-03-14 11:49:08 +0100

    Added admins

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,11 +18,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $admins = ['Nicolas RAUBER', 'Alexis Gybou', 'Karine MOUSDIK'];
+
         // \App\Models\User::factory(10)->create();
         DB::table('tasks')->truncate();
         Task::factory(10)->create();
 
         DB::table('users')->truncate();
-        User::factory(10)->create();
+
+        // Create admins
+        foreach ($admins as $admin) {
+            DB::table('users')->insert(['name'=> $admin,
+                'email'=> str_replace(' ', '.', strtolower($admin)).'@edu.devinci.fr',
+                'email_verified_at' => now(),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'password'=>Hash::make('password'),
+                'remember_token'=> Str::random(10),
+                'api_token'=> Str::random(32)]);
+        }
+        // Other users
+        User::factory(1)->create();
     }
 }
